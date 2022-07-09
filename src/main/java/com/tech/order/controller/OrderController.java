b1fa5bb9fdd262a2bec8707201933ba524531bfb
Ritesh kothale <riteshkothale@gmail.com>
2022-07-09 12:49:54 +0530

    third

--- src/main/java/com/tech/order/controller/OrderController.java
+++ src/main/java/com/tech/order/controller/OrderController.java
@@ -20,7 +20,7 @@ public class OrderController {
 	
 	@PostMapping("/bookOrder")
 	public TransactionResponse bookOrder(@RequestBody TranactionRequest request) {
-		
+		System.out.println("In order service");
 		return orderService.saveOrder(request);
 	}
 }
